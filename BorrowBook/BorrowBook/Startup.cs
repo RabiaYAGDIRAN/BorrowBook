588367d2fb232af8679562e9c0f4ac9fb4088db9
RabiaYAGDIRAN <bflrabia2008@hotmail.com>
2018-02-22 10:09:06 +0300

    OpenProject

---
+++ BorrowBook/BorrowBook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BorrowBook.Startup))]
+namespace BorrowBook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
